ac607547dc636289f67494223c7f53ff3f829f39
Jaeden Fuller <78223047+JFuller1@users.noreply.github.com>
2022-11-24 17:22:27 -0600

    Add celeste to game repository

--- Classes/VideoGameRepository.cs
+++ Classes/VideoGameRepository.cs
@@ -16,10 +16,14 @@ namespace VideoGameRepository.Classes
         public VideoGameRepository()
         {
             Studio coffeeStainStudio = new("Coffee Stain Studios", 1_000_000);
+            Studio maddyMakesGamesStudio = new("Maddy Makes Games", 3_000_000);
             Studios.Add(coffeeStainStudio);
+            Studios.Add(maddyMakesGamesStudio);
 
             Publisher coffeeStainPublishing = new("Coffee Stain Publishing", 1_000_000);
+            Publisher maddyMakesGamesPublishing = new("Maddy Makes Games", 36_000_000);
             Publishers.Add(coffeeStainPublishing);
+            Publishers.Add(maddyMakesGamesPublishing);
 
             VideoGameConsole pc = new("PC", 1500, 20, true);
             Consoles.Add(pc);
@@ -35,13 +39,30 @@ namespace VideoGameRepository.Classes
                 publisher: coffeeStainPublishing,
                 ageRating: 10
                 );
+
+            VideoGames celeste = new(
+                title: "Celeste",
+                category: "Platformer",
+                year: 2018,
+                playerCount: 1,
+                price: 20f,
+                studio: maddyMakesGamesStudio,
+                consoles: new() { pc },
+                publisher: maddyMakesGamesPublishing,
+                ageRating: 10
+                );
+
             VideoGames.Add(satisfactory);
+            VideoGames.Add(celeste);
 
             coffeeStainStudio.AddGame(satisfactory);
             coffeeStainPublishing.AddStudio(coffeeStainStudio);
+
+            maddyMakesGamesStudio.AddGame(celeste);
+            maddyMakesGamesPublishing.AddStudio(maddyMakesGamesStudio);
+
             pc.AddGame(satisfactory);
-
-
+            pc.AddGame(celeste);
         }
     }
 }
