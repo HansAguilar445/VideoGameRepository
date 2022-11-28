0d206a0a8558287a83d6db49bf2893fa0f6335f1
erunions <78222901+erunions@users.noreply.github.com>
2022-11-28 08:29:13 -0600

    add risk of rain, risk of rain 2, bloons td 6 to videogamerepository.cs

--- Classes/VideoGameRepository.cs
+++ Classes/VideoGameRepository.cs
@@ -41,7 +41,67 @@ namespace VideoGameRepository.Classes
             coffeeStainPublishing.AddStudio(coffeeStainStudio);
             pc.AddGame(satisfactory);
 
+            Studio hopooGames = new("Hopoo Games", 5_000_000);
+            Studios.Add(hopooGames);
 
+            Publisher gearbox = new("Gearbox", 5);
+            Publishers.Add(gearbox);
+
+            VideoGame riskOfRain = new(
+                title: "Risk of Rain",
+                category: "Roguelike",
+                year: 2013,
+                playerCount: 4,
+                price: 11f,
+                studio: hopooGames,
+                consoles: new() { pc },
+                publisher: gearbox,
+                ageRating: 13
+                );
+            VideoGames.Add(riskOfRain);
+
+            hopooGames.AddGame(riskOfRain);
+            gearbox.AddStudio(hopooGames);
+            pc.AddGame(riskOfRain);
+
+            VideoGame riskOfRain2 = new(
+                title: "Risk of Rain 2",
+                category: "Roguelike",
+                year: 2018,
+                playerCount: 4,
+                price: 29f,
+                studio: hopooGames,
+                consoles: new() { pc },
+                publisher: gearbox,
+                ageRating: 13
+                );
+            VideoGames.Add(riskOfRain2);
+
+            hopooGames.AddGame(riskOfRain2);
+            pc.AddGame(riskOfRain2);
+
+            Studio ninjaKiwi = new("Ninja Kiwi", 3_000_000);
+            Studios.Add(ninjaKiwi);
+
+            Publisher ninjaKiwiPublishing = new("Ninja Kiwi Publishing", 1_000_000);
+            Publishers.Add(ninjaKiwiPublishing);
+
+            VideoGame bloonsTD6 = new(
+                title: "Bloons TD 6",
+                category: "Tower Defense",
+                year: 2018,
+                playerCount: 4,
+                price: 15.49f,
+                studio: ninjaKiwi,
+                consoles: new() { pc },
+                publisher: ninjaKiwiPublishing,
+                ageRating: 9
+                );
+            VideoGames.Add(bloonsTD6);
+
+            ninjaKiwi.AddGame(bloonsTD6);
+            ninjaKiwiPublishing.AddStudio(ninjaKiwi);
+            pc.AddGame(bloonsTD6);
         }
     }
 }
